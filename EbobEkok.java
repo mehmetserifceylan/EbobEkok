d0b9177e0310c5516135890b64b8b7c1c1473faa
mehmetserifceylan <65469373+mehmetserifceylan@users.noreply.github.com>
2023-01-18 17:31:55 +0300

    Add files via upload

---
+++ EbobEkok.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class EbobEkok {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("n1 sayısını giriniz:");
+        int n1=input.nextInt();
+
+        System.out.print("n2 sayısını giriniz:");
+        int n2=input.nextInt();
+        int ebob=1;
+        int k=n1;
+        while (k>=1){
+            if (n1%k==0 && n2%k==0){
+                ebob=k;
+                break;
+            }
+            k--;
+        }
+        System.out.println(ebob);
+        int i=1;
+        while ((n1*n2)>=i){
+            if (i%n1==0 && i%n2==0){
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
+
+
+
+        /*for(int k=n1; k>=1; k--){
+            if (n1%k==0 && n2%k==0){
+                ebob=k;
+                break;
+            }
+        }
+
+        for (int i = 1; i <=(n1*n2) ; i++) {
+            if (i%n1==0 && i%n2==0){
+                System.out.println(i);
+                break;
+            }
+        }
+        System.out.println((n1*n2)/ebob);*/
+
+        /*for (int i = 1; i <=n1 ; i++) {
+            if (n1%i==0 && n2%i==0){
+                ebob=i;
+            }
+        }
+        System.out.println(ebob);*/
+
+
+    }
+}
